845f6b7e33f7ed7ad8f974911fb9e8725e4df0b3
Wladyslaw <vladislavhrabryk@gmail.com>
2022-05-19 16:02:46 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+const storeDescription = {
+    budget:10000};
+storeDescription.employees = ['a','b','c'];
+storeDescription.products = {towar:'1', tower:'2'};
+storeDescription.open = true;
+
+console.log(storeDescription);
+
+let number = 25;
+number = 24;
+console.log(number);
