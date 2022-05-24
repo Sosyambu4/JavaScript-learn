1346f079b2b63ac2c309114e82ae797982449458
Wladyslaw <vladislavhrabryk@gmail.com>
2022-05-24 22:31:14 +0100

    3 commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,72 @@
 "use strict";
 
 
-const numberOfFilms = prompt(
-    'Сколько фильмов вы уже посмотрели?');
-console.log(numberOfFilms);
+let numberOfFilms;
 
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
- 
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
 
+    }
+}
+start();
 
-let personalMovieDB = {
-    count:(numberOfFilms),
+const personalMovieDB = {
+    count:numberOfFilms,
     movies:{},
     actors:{},
     genres:[],
-    privat: false,
+    privat: false
 };
 
-const  a = prompt('Одни из последних просмотреных фильмов?',''),
-       b = prompt('Как вы оцениваете его?',''),
-       c = prompt('Одни из последних просмотреных фильмов?',''),
-       d = prompt('Как вы оцениваете его?','');
+function rememberMyFilms() {
+    for ( let i = 0; i < 1; i++) {
+        const a = prompt('Один из последих просмотреных фильмов?',""),
+              b = prompt('На сколько оцените его?',"");
+    
+        if (a != null && b !=null && a != '' && b !='' && a.length < 50){
+          personalMovieDB.movies[a] = b;
+          console.log('done');  
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function detectPersonalLevel() {
+    if(personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log('Вы классный зритель');
+    } else if (personalMovieDB.count <= 30){
+        console.log('Вы киноман');}
+    else {
+        console.log('Error 404');
+    }
+}
+detectPersonalLevel();
 
-console.log(personalMovieDB);
+function writeYourGenres(){
+    for ( let i = 1; i < 4; i++){
+        const a = prompt(`Ваш любимый жaнр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = a; 
+        
+    }
+};
+ 
+writeYourGenres();
+
+
+function showMyDB(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
+
+
